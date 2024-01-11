47c64a4ed1f727feb047f201d09378f8d2e0072d
andr3oix <72841646+andr3oix@users.noreply.github.com>
2024-01-11 19:08:59 +0300

    Update nokogiri-1.14.5.sh

--- nokogiri-1.14.5.sh
+++ nokogiri-1.14.5.sh
@@ -1,39 +1,35 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-# Dependencies Installation
-center "* Dependencies installation..."
-pkg update -y
-pkg upgrade -y -o Dpkg::Options::="--force-confnew"
-pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
-apt autoremove -y
-
 # Fix ruby BigDecimal
-center "* Fix ruby BigDecimal"
-#source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
-wget https://github.com/andr3oix/nokogiri/releases/download/Nokogiri/nokogiri-1.14.5.tar.gz
-cd $HOME;tar xf nokogiri-1.14.5.tar.gz;rm $HOME/nokogiri-1.14.5.tar.gz*
+source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
 
-# Fix nokogiri rubygem
+# Fix Metasploit-Framework
+cd $PREFIX/opt/metasploit-framework/
+wget https://github.com/andr3oix/nokogiri/releases/download/Nokogiri/lib.tar.gz
+tar xf lib.tar.gz
+rm lib.tar.gz
+cd $HOME
+echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m\e[0;37m"
+sleep 2.0
+
+# Fix libxml-So
+cd $PREFIX/lib/
+wget https://github.com/andr3oix/nokogiri/releases/download/Nokogiri/so.tar.gz
+tar xf so.tar.gz
+rm so.tar.gz
+cd $HOME
+echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m\e[0;37m"
+sleep 2.0
+
+# Fix Nokogiri
+cd $PREFIX/include/
+wget https://github.com/andr3oix/nokogiri/releases/download/Nokogiri/libxml.tar.gz
+tar xf libxml.tar.gz
+rm libxml.tar.gz*
+cd $HOME
+echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m\e[0;37m"
+sleep 2.0
+
+# Fix RubyGems
 bundle config build.nokogiri "--use-system-libraries --with-xml2-include=${PREFIX}/include/libxml2"
-rm -rf $PREFIX/include/;rm -rf $PREFIX/opt/metasploit-framework/lib;rm $PREFIX/lib/libxml2.so*;rm $PREFIX/lib/libxslt.so
-
-# Building 
-cp -r $HOME/.nokogiri/nokogiri-1.14.5/* $PREFIX/
-cp $HOME/.nokogiri/so/* $PREFIX/lib/
-cp -r $HOME/.nokogiri/resolv/* $PREFIX/opt/metasploit-framework/
-
-
-# Link Metasploit Executables
-ln -sf ${PREFIX}/opt/metasploit-framework/msfconsole ${PREFIX}/bin/
-ln -sf ${PREFIX}/opt/metasploit-framework/msfvenom ${PREFIX}/bin/
-ln -sf ${PREFIX}/opt/metasploit-framework/msfrpcd ${PREFIX}/bin/
-termux-elf-cleaner ${PREFIX}/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
-
-echo -e "\033[32m" # Blue color
-center "Installation complete"
-echo -e "\nDone"
-echo -e "\033[0m" # Reset color
- # Blue color
-center "Installation complete"
-echo -e "\nStart Metasploit using the command: msfconsole"
-echo -e "\033[0m" # Reset color
+echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m\e[0;37m"
+sleep 2.0
